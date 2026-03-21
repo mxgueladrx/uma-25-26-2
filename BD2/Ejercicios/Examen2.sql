4a6f42f2976fb4380271d455eef8d49a7f138525
mxgueladrx <gheorghemigueladrian@gmail.com>
2026-03-21 12:30:50 +0100

    ejercicio grupal 2 rc1 hecho y ejemplo examen 2 bd2 hecho

--- BD2/Ejercicios/Examen2.sql
+++ BD2/Ejercicios/Examen2.sql
@@ -64,7 +64,7 @@ CREATE OR REPLACE TRIGGER TR_VISTAS_ERRORES
 AFTER UPDATE OF NUEVO ON TB_VISTAS
 FOR EACH ROW
 BEGIN
-    IF :NEW.NUEVO = "ERROR" THEN
+    IF :NEW.NUEVO = 'ERROR' THEN
         INSERT INTO TB_ERRORES VALUES (USER, :OLD.ANTERIOR, SYSDATE);
     END IF;
 END;
@@ -78,20 +78,63 @@ poniéndole el prefijo TB_. Así, por ejemplo, si aparece A1, debe ejecutar la i
 A1 TO TB_A1. Además, en la tabla TB_TABLAS se guardará el nombre nuevo y la fecha en
 la que se ha hecho el cambio. */
 
-CREATE PACKAGE PK_ESTILO AS
+/* Ejercicio 3 (1 puntos). Dentro del paquete PK_ESTILO crea con un procedimiento similar al
+anterior denominado PR_DESHACER que no recibe parámetros. El procedimiento debe
+recorrer la tabla TB_TABLAS y buscar la tabla que aparece en la columna NUEVO. El
+procedimiento debe renombrarla con el nombre anterior. Así, por ejemplo, si aparece C1 en
+anterior y TB_C1 en nuevo, debe ejecutar la instrucción RENAME TB_C1 TO C1. Además,
+en la tabla TB_TABLAS el valor NUEVO se pondrá a NULL y se almacena la fecha en la que se
+ha hecho el cambio. Si el cambio no se puede hacer, se controlará la excepción y se modificará
+la columna NUEVO con palabra ‘ERROR’ en la fila correspondiente a la vista que ha dado el
+error. */
+
+CREATE OR REPLACE PACKAGE PK_ESTILO AS
     PROCEDURE PR_RENOMBRA_TABLAS;
+    PROCEDURE PR_DESHACER;
 END;
 /
 
-CREATE PACKAGE BODY PK_ESTILO AS
+CREATE OR REPLACE PACKAGE BODY PK_ESTILO AS
     PROCEDURE PR_RENOMBRA_TABLAS IS
-        CURSOR TABLAS IS SELECT ANTERIOR FROM TB_TABLAS FOR UPDATE;
+        CURSOR C_TABLAS IS SELECT * FROM TB_TABLAS;
+        V_NUEVO VARCHAR2(30);
     BEGIN
-        FOR C IN TABLAS LOOP
-            
-        END LOOP;
+        FOR C IN C_TABLAS LOOP
+            IF C.ANTERIOR NOT LIKE 'TB_%' AND C.NUEVO IS NULL THEN
+                V_NUEVO := 'TB_' || C.ANTERIOR;
+                EXECUTE IMMEDIATE 'RENAME ' || C.ANTERIOR || ' TO ' || V_NUEVO;
 
+                UPDATE TB_TABLAS
+                SET NUEVO = V_NUEVO, FECHA = SYSDATE
+                WHERE ANTERIOR = C.ANTERIOR;
+                COMMIT;
+            END IF;
+        END LOOP;
     END PR_RENOMBRA_TABLAS;
 
+    PROCEDURE PR_DESHACER IS
+        CURSOR C_TABLAS IS SELECT * FROM TB_TABLAS;
+    BEGIN
+        FOR C IN C_TABLAS LOOP
+            BEGIN
+                EXECUTE IMMEDIATE 'RENAME ' || C.NUEVO || ' TO ' || C.ANTERIOR;
+
+                UPDATE TB_TABLAS
+                SET NUEVO = NULL, FECHA = SYSDATE
+                WHERE ANTERIOR = C.ANTERIOR;
+            EXCEPTION
+                WHEN OTHERS THEN
+                    UPDATE TB_TABLAS
+                    SET NUEVO = 'ERROR', FECHA = SYSDATE
+                    WHERE ANTERIOR = C.ANTERIOR;
+            END;
+            COMMIT;
+        END LOOP;
+    END PR_DESHACER;
 END;
 /
+
+SELECT * FROM TB_TABLAS;
+SELECT * FROM USER_TABLES;
+EXECUTE PK_ESTILO.PR_RENOMBRA_TABLAS;
+EXECUTE PK_ESTILO.PR_DESHACER;
